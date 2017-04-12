f9fc05d2c7d272c4773f86f93bc130300d4f9d51
ivmachiavelli <ivmachiavelli@protonmail.com>
2017-04-12 23:02:04 +0000

    Removed i3status, Added quick.sh

---
+++ quick.sh
@@ -0,0 +1,1 @@
+sudo pacman -Syyu -y && sudo pacman -S wireshark-gtk etherape firefox vivaldi-stable chromium atom nvim cherrytree clamav vlc gimp keepassx2 go i3 rofi compton lxappearance htop nethogs docker nmap  
