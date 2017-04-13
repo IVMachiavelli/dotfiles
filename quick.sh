386c75bb430e631882eb632cbb8f03c37582650c
ivmachiavelli <ivmachiavelli@protonmail.com>
2017-04-13 06:32:34 +0000

    Added a few packages

--- quick.sh
+++ quick.sh
@@ -1,1 +1,1 @@
-sudo pacman -Syyu -y && sudo pacman -S wireshark-gtk zsh etherape firefox vivaldi-stable chromium atom nvim cherrytree clamav vlc gimp keepassx2 go i3 rofi compton lxappearance htop nethogs docker nmap -y 
+sudo pacman -Syyu -y && sudo pacman -S wireshark-gtk zsh etherape firefox vivaldi-stable chromium atom nvim cherrytree clamav vlc gimp keepassx2 go i3 rofi compton feh lxappearance htop nethogs docker nmap gparted libreoffice-fresh -y 
