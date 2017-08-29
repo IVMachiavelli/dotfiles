69c6cd63b009da0f9e1ff6ffad879815afc90e3c
ivmachiavelli <ivmachiavelli@protonmail.com>
2017-08-29 05:31:54 -0600

    Updated Rice

--- quick.sh
+++
@@ -1,1 +0,0 @@
-zsh firefox vivaldi-stable chromium atom nvim cherrytree clamav vlc gimp keepassx2 go i3 rofi compton feh lxappearance htop nethogs docker nmap gparted libreoffice-fresh nmap zathura ranger
