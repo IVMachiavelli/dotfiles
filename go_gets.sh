78150572d05867d8ab69c8109e7058051fb93211
ivmachiavelli <IVMachiavelli@protonmail.com>
2017-09-21 12:27:01 -0400

    Added Common Golang packages Script

---
+++ go_gets.sh
@@ -0,0 +1,7 @@
+# Go get all my common go repo's
+go get -u github.com/pilu/fresh
+go get -u github.com/PuerkitoBio/goquery
+go get -u github.com/gorilla/mux
+go get -u github.com/golang-web-dev
+go get -u github.com/GolangTraining
+
